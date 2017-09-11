951d7e37f557ef64ce01c3a294b717f7fe54e4b1
superlintball <ravnikator@gmail.com>
2017-09-11 11:32:49 -0700

    added comments

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,7 +1,7 @@
 /*
- * author: Raveen Karnik
+ * Author: Raveen Karnik
  * date: 9-7-17
- * This program creates a guessing game for the player to guess the game
+ * This program creates a guessing game for the player to guess a number  
  */
 
 #include <iostream>
@@ -10,17 +10,28 @@
 
 using namespace std;
 
+//main function to run program
 int main()
 {
+	//variable to keep track of if the user wants to keep playing
 	bool playing = true;
 	while (playing)
 	{
+		//initialize random number function to current timestamp
 		srand(time(NULL));
 		int randNum = rand() % 101;
 		cout << "The computer has chosen a number. Try to guess it." << endl;
+		//initialize the first guess to an impossible option
 		int guess = -5;
- 		cin >> guess;
+		cin >> guess;
+		while (guess < 0 || guess > 100)
+		{
+			cout << "Your guess must be between 0 and 100, inclusive. Guess again." << endl;
+			cin >> guess;
+		}
+		//initialize a variable to keep track of the number of guesses
 		int guesses = 1;
+		//have the user guess the random number, telling them if it's too high or too low every time
 		while (guess != randNum)
 		{
 			guesses++;
@@ -34,8 +45,10 @@ int main()
 			}
 			cin >> guess;
 		}
+		//announce to the user that they succeeded and what the number was. Tell them how many guesses it took then ask if they want to play again
 		cout << "Congratulations! The computer's number was " << randNum << "!" << endl;
 		cout << "It took you " << guesses << " guesses. Play again? (y/n)" << endl;
+		//ask the user if they want to keep playing. Switch the playing variable to false if they say no
 		char keepPlaying = 'w';
 		cin >> keepPlaying;
 		while (!(keepPlaying == 'y' || keepPlaying == 'n' || keepPlaying  == 'Y' || keepPlaying == 'N'))
